d41e0cc9c2ef8e4eb6262f4b84badb45e5032b33
test0testa <134169565+test0testa@users.noreply.github.com>
2023-05-20 22:19:22 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
     setLoading(true);
     axios
-      .post("http://127.0.0.1:5500/chat", {   // Lokal hosti jem
+      .post("https://test0testa.github.io/chat", {   // Lokal hosti jem
         question,
       })
       .then((response) => {
